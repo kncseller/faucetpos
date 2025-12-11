0e068d553f29b49b90363b3c25279752fad3097f
kncseller <kncseller@gmail.com>
2025-12-11 17:36:48 +0700

    b

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,60 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
 
-export const protocol =
-  process.env.NODE_ENV === 'production' ? 'https' : 'http';
-export const rootDomain =
-  process.env.NEXT_PUBLIC_ROOT_DOMAIN || 'localhost:3000';
+// import { clsx, type ClassValue } from 'clsx';
+// import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+// export const protocol =
+//   process.env.NODE_ENV === 'production' ? 'https' : 'http';
+// export const rootDomain =
+//   process.env.NEXT_PUBLIC_ROOT_DOMAIN || 'localhost:3000';
+
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs));
+// }
+
+
+ 
+const domains = {
+  "/":"pos/",
+  "cafe":"pos/",
+  "food":"pos/",
+  "shop":"pos/",
+  "karaoke":"pos/",
+  "bida":"pos/" ,
+  "chat":"chat/" 
+};
+
+
+export function extractSubdomain(request: NextRequest): string | null {
+  const url = request.url;
+  const host = request.headers.get('host') || '';
+  const hostname = host.split(':')[0];
+
+  let subdomain = "/";
+
+  if (hostname.includes('.vercel.app')) {
+    return subdomain;
+  }
+
+  
+  
+  // Local development environment
+  if (url.includes('localhost') || url.includes('127.0.0.1')) {
+     
+    if (hostname.includes('.localhost')) {
+      subdomain =  hostname.split('.')[0];
+    }
+
+    
+  }else{
+    subdomain =  hostname.includes('.')?hostname.split('.').slice(0, -2).join('.'):null;
+  }
+  
+  if(subdomain){
+  if(subdomain.includes("admin")){
+       subdomain = "admin";
+    }else{
+       subdomain = domains[subdomain]?domains[subdomain]:domains["/"];
+    }
+  }
+  return subdomain;
 }
--- middleware.ts
+++ middleware.ts
@@ -1,70 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server';
-// import { rootDomain } from '@/lib/utils';
-
-// const appconfig={
-//    domainapi :'https://f7.donggiatri.com/users/demo/pluto/'
-// };
-
-const domains = {
-  "/":"pos/",
-  "cafe":"pos/",
-  "food":"pos/",
-  "shop":"pos/",
-  "karaoke":"pos/",
-  "bida":"pos/" 
-};
-
-function extractSubdomain(request: NextRequest): string | null {
-  const url = request.url;
-  const host = request.headers.get('host') || '';
-  const hostname = host.split(':')[0];
-
-
-  if (hostname.includes('.vercel.app')) {
-    return "/";
-  }
+import { extractSubdomain } from '@/lib/utils';
 
   
-  
-  // Local development environment
-  if (url.includes('localhost') || url.includes('127.0.0.1')) {
-    // Try to extract subdomain from the full URL
-    const fullUrlMatch = url.match(/http:\/\/([^.]+)\.localhost/);
-    if (fullUrlMatch && fullUrlMatch[1]) {
-      return fullUrlMatch[1];
-    }
-
-    // Fallback to host header approach
-    if (hostname.includes('.localhost')) {
-      return hostname.split('.')[0];
-    }
-
-
-    return null;
-  }
-
-  return hostname.includes('.')?hostname.split('.').slice(0, -2).join('.'):null;
-
-  // // Production environment
-  // const rootDomainFormatted = rootDomain.split(':')[0];
-
-  // // Handle preview deployment URLs (tenant---branch-name.vercel.app)
-  // if (hostname.includes('---') && hostname.endsWith('.vercel.app')) {
-  //   const parts = hostname.split('---');
-  //   return parts.length > 0 ? parts[0] : null;
-  // }
-
-
-
-  // // Regular subdomain detection
-  // const isSubdomain =
-  //   hostname !== rootDomainFormatted &&
-  //   hostname !== `www.${rootDomainFormatted}` &&
-  //   hostname.endsWith(`.${rootDomainFormatted}`);
-
-  // return isSubdomain ? hostname.replace(`.${rootDomainFormatted}`, '') : null;
-}
-
 export async function middleware(request: NextRequest) {
   const origin = request.headers.get('origin');
   // const country = request.geo?.country || 'US';
@@ -92,22 +29,14 @@ export async function middleware(request: NextRequest) {
 
   const { pathname } = request.nextUrl;
   let subdomain = extractSubdomain(request);
-
+  
 
   // On the root domain, allow normal access
   let response= NextResponse.next();
 
   if (subdomain) {
 
-
-    if(subdomain.includes("admin")){
-       subdomain = "admin";
-    }else{
-       subdomain = domains[subdomain]?domains[subdomain]:domains["/"];
-    }
-
-   
-
+ 
     // Block access to admin page from subdomains
     if (pathname.startsWith('/admin')) {
       response =  NextResponse.redirect(new URL('/', request.url));
@@ -115,15 +44,16 @@ export async function middleware(request: NextRequest) {
       // For the root path on a subdomain, rewrite to the subdomain page
       response = NextResponse.rewrite(new URL(`/s/${subdomain}`, request.url));
     }else if (pathname.startsWith('/api/')) { 
-      //detail api
-      response = NextResponse.json(new URL(`/s/${subdomain}/api/index/`, request.url));
-    }
+      console.log(request.url,` => /s/${subdomain}/api/index/`,pathname);
+      //detail api in subdomain
+      response = NextResponse.redirect(new URL(`/s/${subdomain}/${pathname}`, request.url));
+    } 
   }else if (pathname != '/') {
     //detail api
     if (pathname.startsWith('/api/')) { 
       response = NextResponse.json(new URL('/api/index/', request.url));
     }else{
-      response = NextResponse.rewrite(new URL('/', request.url));
+      response = NextResponse.redirect(new URL('/', request.url));
     } 
       
   }
@@ -137,13 +67,13 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
+  matcher: [ 
     /*
      * Match all paths except for:
      * 1. /api routes
      * 2. /_next (Next.js internals)
      * 3. all root files inside /public (e.g. /favicon.ico)
      */
-    '/((?!api|_next|build|s|[\\w-]+\\.\\w+).*)'
+    '/((?!_next|build|s|[\\w-]+\\.\\w+).*)'
   ]
 };
